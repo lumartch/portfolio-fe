c16e67836e9635519614458a03b50be31c1e8845
lumartch <lumartch@gmail.com>
2023-09-10 16:39:40 +0200

    Addind Head title.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-
+import Head from 'next/head';
 import type { AppProps } from 'next/app'
 
 import { Layout, Theme } from '@/components';
@@ -7,6 +7,9 @@ import '@/styles/globals.css';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Theme>
+        <Head>
+          <title>Luis Martínez - Portfolio</title>
+        </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
