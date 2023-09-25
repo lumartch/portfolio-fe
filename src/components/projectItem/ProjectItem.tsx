900d0c8770760f61a0e12f7b0abe44d63787e480
lumartch <lumartch@gmail.com>
2023-09-25 22:35:13 +0200

    Added toaster

--- src/components/projectItem/ProjectItem.tsx
+++ src/components/projectItem/ProjectItem.tsx
@@ -1,18 +1,29 @@
-import { FC } from 'react';
-import { Box, Button, Card, CardActions, CardContent, Chip, Grid, Typography, useTheme } from '@mui/material';
+import { FC, useState } from 'react';
+import { Alert, Box, Button, Card, CardActions, CardContent, Chip, Grid, Snackbar, Typography, useTheme } from '@mui/material';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 import { IProject } from '@/interfaces/';
 import { EGitSource } from '@/const';
-
 type IProjectItem = {
     project: IProject;
 }
 
 export const ProjectItem: FC<IProjectItem> = ({ project }) => {
     const theme = useTheme();
+    const [open, setOpen] = useState<boolean>(false);
     const { id, name, full_name, clone_url, created_at, default_branch, git_url, html_url, ssh_url, source } = project;
+
+    const handleCopiedToClipBoard = () => setOpen(true);
+
+    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
+
     return (
+    <>
         <Card variant={ theme.palette.mode === 'dark' ? 'elevation' : 'outlined' }>
             <CardContent>
                 <Box sx={{rowGap: 2}}>
@@ -31,12 +42,12 @@ export const ProjectItem: FC<IProjectItem> = ({ project }) => {
             <CardActions>
                 <Box sx={{ display: 'flex', width: '100%', justifyContent: 'space-evenly' }}>
                     <CopyToClipboard text={git_url}>
-                        <Button color='primary'>
+                        <Button color='primary' onClick={handleCopiedToClipBoard}>
                             Git URL
                         </Button>
                     </CopyToClipboard>
                     <CopyToClipboard text={ssh_url}>
-                        <Button color='primary'>
+                        <Button color='primary' onClick={handleCopiedToClipBoard}>
                             SSH
                         </Button>
                     </CopyToClipboard>
@@ -44,7 +55,7 @@ export const ProjectItem: FC<IProjectItem> = ({ project }) => {
                         source === EGitSource.GITHUB 
                         ? 
                             <CopyToClipboard text={clone_url}>
-                                <Button color='primary'>
+                                <Button color='primary' onClick={handleCopiedToClipBoard}>
                                     URL
                                 </Button>
                             </CopyToClipboard>
@@ -57,5 +68,11 @@ export const ProjectItem: FC<IProjectItem> = ({ project }) => {
                 </Box>
             </CardActions>
         </Card>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity='success' sx={{ width: '100%' }}>
+                Value copied to the clipboard!
+            </Alert>
+        </Snackbar>
+    </>
     );
 }
