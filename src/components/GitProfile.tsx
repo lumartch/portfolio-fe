f93e7a888940974529a1553d1c5ef776d2719788
Lumart Chryssomallis <lumartch@gmail.com>
2025-01-23 19:22:17 +0100

    Removing Git profile picture from git projects.

--- src/components/GitProfile.tsx
+++ src/components/GitProfile.tsx
@@ -1,23 +1,17 @@
 import { ColorMode } from '@/enums';
 import { IProfile } from '@/interfaces';
-import { Button, Card, CardActions, CardContent, CardMedia, Typography, useTheme } from '@mui/material';
+import { Button, Card, CardActions, CardContent, Typography, useTheme } from '@mui/material';
 import React from 'react';
 
 type Props = IProfile;
 
-export const GitProfile: React.FC<Props> = ({ avatar_uri, bio, git_uri, name, user_id, username } ) => {
+export const GitProfile: React.FC<Props> = ({ bio, git_uri, name, user_id, username } ) => {
     const { palette } = useTheme();
 
     const varaint = palette.mode === ColorMode.DARK ? 'elevation' : 'outlined'; 
 
     return (
         <Card sx={{ width: '100%' }} variant={ varaint }>
-            <CardMedia
-                alt={`${username}'s profile image.`}
-                component='img'
-                height={200}
-                image={avatar_uri}
-            />
             <CardContent>
                 <Typography variant='h5'>{name}</Typography>
                 <Typography color='text.secondary' variant='subtitle1'>
